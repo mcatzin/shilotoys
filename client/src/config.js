92b88f1304f9ae89af1201ed5dead6ebfc952655
mcatzin <catzin21@gmail.com>
2020-12-07 23:40:43 -0800

    edit config

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,2 @@
 /* eslint-disable import/prefer-default-export */
-export const apiUrl = document.location.href.startsWith('https://localhost') ? 'http://localhost:5000' : '';
+export const apiUrl = document.location.href.startsWith('http://localhost') ? 'http://localhost:5000' : '';
